36bdd3b933e3919ef2189d057390e562d4a12271
Kyrin Betts <actions@github.com>
2025-09-18 15:43:48 -0500

    Initial push

---
+++ src/main/java/com/kr1s1s/subtlyd/SubtlyDungeonsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.kr1s1s.subtlyd;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SubtlyDungeonsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
