a28838aeee56f74e6b34ec55332dcdc4033c42ff
Kyrin Betts <actions@github.com>
2025-10-04 01:19:37 -0500

    Updated from deprecated method

--- src/main/java/com/kr1s1s/subtlyd/SubtlyDungeonsClient.java
+++ src/main/java/com/kr1s1s/subtlyd/SubtlyDungeonsClient.java
@@ -4,14 +4,14 @@ import com.kr1s1s.subtlyd.mobs.zombie.DrownedRendererSD;
 import com.kr1s1s.subtlyd.mobs.zombie.HuskRendererSD;
 import com.kr1s1s.subtlyd.mobs.zombie.ZombieRendererSD;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.EntityRendererRegistry;
+import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.world.entity.EntityType;
 
 public class SubtlyDungeonsClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
-        EntityRendererRegistry.register(EntityType.ZOMBIE, ZombieRendererSD::new);
-        EntityRendererRegistry.register(EntityType.HUSK, HuskRendererSD::new);
-        EntityRendererRegistry.register(EntityType.DROWNED, DrownedRendererSD::new);
+        EntityRenderers.register(EntityType.ZOMBIE, ZombieRendererSD::new);
+        EntityRenderers.register(EntityType.HUSK, HuskRendererSD::new);
+        EntityRenderers.register(EntityType.DROWNED, DrownedRendererSD::new);
     }
 }
