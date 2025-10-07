994fc67326ac48ef059b853a25ffe41360504748
Kyrin Betts <actions@github.com>
2025-10-07 11:30:08 -0500

    Fixed tent name displaying

--- src/main/java/com/kr1s1s/subtlyd/SubtlyDungeonsDataGenerator.java
+++ src/main/java/com/kr1s1s/subtlyd/SubtlyDungeonsDataGenerator.java
@@ -1,11 +1,14 @@
 package com.kr1s1s.subtlyd;
 
+import com.kr1s1s.subtlyd.world.item.RecipeProviderSD;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class SubtlyDungeonsDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
+        pack.addProvider(RecipeProviderSD::new);
 	}
 }
