3e0cc68053f903dfa1d9d525a995623078366cfa
Kyrin Betts <actions@github.com>
2025-10-30 15:22:25 -0500

    Refactor

--- src/main/java/com/kr1s1s/subtlyd/mixin/server/block/TallGrassBlockMixin.java
+++ src/main/java/com/kr1s1s/subtlyd/mixin/server/block/TallGrassBlockMixin.java
@@ -1,21 +1,16 @@
 package com.kr1s1s.subtlyd.mixin.server.block;
 
 import com.kr1s1s.subtlyd.world.block.BlocksSD;
-import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.level.LevelReader;
 import net.minecraft.world.level.ScheduledTickAccess;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.TallGrassBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
