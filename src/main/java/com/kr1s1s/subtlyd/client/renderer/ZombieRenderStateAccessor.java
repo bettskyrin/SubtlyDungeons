c688d4fa8a8ffbcded414fc6c6651a03d29483a0
Kyrin Betts <actions@github.com>
2025-12-21 17:16:54 -0600

    refactor: Updated zombie renderer mixins for compatibility

---
+++ src/main/java/com/kr1s1s/subtlyd/client/renderer/ZombieRenderStateAccessor.java
@@ -0,0 +1,7 @@
+package com.kr1s1s.subtlyd.client.renderer;
+
+
+public interface ZombieRenderStateAccessor {
+    boolean subtlyDungeons$isLeader();
+    void subtlyDungeons$setLeader(boolean isLeader);
+}
