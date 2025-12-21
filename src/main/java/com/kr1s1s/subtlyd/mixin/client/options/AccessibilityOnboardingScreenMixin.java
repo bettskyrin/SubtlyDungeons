f6c8113d80c75f19f39fe7504f0c503df75fcdac
Kyrin Betts <actions@github.com>
2025-12-21 17:18:39 -0600

    fix: Fixed accessibility onboarding repeated openings

--- src/main/java/com/kr1s1s/subtlyd/mixin/client/options/AccessibilityOnboardingScreenMixin.java
+++ src/main/java/com/kr1s1s/subtlyd/mixin/client/options/AccessibilityOnboardingScreenMixin.java
@@ -4,14 +4,17 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screens.AccessibilityOnboardingScreen;
 import net.minecraft.client.gui.screens.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(AccessibilityOnboardingScreen.class)
 public class AccessibilityOnboardingScreenMixin {
+    @Shadow private void close(final boolean onboardingFinished, final Runnable runnable) {}
+
     @Inject(method = "onClose", at = @At("TAIL"))
     private void setNewTitle(CallbackInfo ci) {
-        Minecraft.getInstance().setScreen(new TitleScreen(true));
+        close(true, () -> Minecraft.getInstance().setScreen(new TitleScreen(true)));
     }
 }
