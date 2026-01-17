291cd4f46623143b230cebaeee058c42eb8a6655
Kyrin Betts <actions@github.com>
2026-01-17 13:17:37 -0600

    chore: Removed unused code

--- src/main/java/com/kr1s1s/subtlyd/util/init/ClientInitializerSD.java
+++ src/main/java/com/kr1s1s/subtlyd/util/init/ClientInitializerSD.java
@@ -4,7 +4,6 @@ import com.kr1s1s.subtlyd.client.ClientTickEventsSD;
 import com.kr1s1s.subtlyd.client.renderer.EntityRenderersSD;
 import com.kr1s1s.subtlyd.client.renderer.state.BlockRendering;
 import com.kr1s1s.subtlyd.sounds.SoundEventsSD;
-import com.kr1s1s.subtlyd.world.level.block.ColorProviderSD;
 import net.fabricmc.api.ClientModInitializer;
 
 public class ClientInitializerSD implements ClientModInitializer {
@@ -14,6 +13,5 @@ public class ClientInitializerSD implements ClientModInitializer {
         SoundEventsSD.registration();
         ClientTickEventsSD.registration();
         BlockRendering.init();
-        ColorProviderSD.init();
     }
 }
