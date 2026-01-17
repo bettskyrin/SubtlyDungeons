ba5034e129e40c908212657575176c514acbffff
Kyrin Betts <actions@github.com>
2026-01-17 12:57:11 -0600

    Fabric API version bump

--- src/main/java/com/kr1s1s/subtlyd/world/level/block/ColorProviderSD.java
+++ src/main/java/com/kr1s1s/subtlyd/world/level/block/ColorProviderSD.java
@@ -1,16 +1,15 @@
 package com.kr1s1s.subtlyd.world.level.block;
 
 import com.kr1s1s.subtlyd.world.block.BlocksSD;
-import net.fabricmc.fabric.api.client.rendering.v1.ColorProviderRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.BlockColorRegistry;
 import net.minecraft.client.renderer.BiomeColors;
 
 public class ColorProviderSD {
     public static void init() {
-        ColorProviderRegistry.BLOCK.register(((blockState, blockAndTintGetter, blockPos, i) -> {
+        BlockColorRegistry.register(((_, blockAndTintGetter, blockPos, _) -> {
             if (blockAndTintGetter == null || blockPos == null) {
                 return -1;
             }
-
             return BiomeColors.getAverageFoliageColor(blockAndTintGetter, blockPos);
         }), BlocksSD.REEDS);
     }
