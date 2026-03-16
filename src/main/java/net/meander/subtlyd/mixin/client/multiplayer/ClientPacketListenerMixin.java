6497b952ff191ac7882bee9afac74b482ed4867e
Kyrin Betts <actions@github.com>
2026-03-16 15:48:06 -0500

    feat: Updated screen shake from explosion method

---
+++ src/main/java/net/meander/subtlyd/mixin/client/multiplayer/ClientPacketListenerMixin.java
@@ -0,0 +1,20 @@
+package net.meander.subtlyd.mixin.client.multiplayer;
+
+import net.meander.subtlyd.util.CameraShake;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.network.protocol.game.ClientboundExplodePacket;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPacketListener.class)
+public class ClientPacketListenerMixin {
+    /**
+     * Handles camera shake for explosions.
+     */
+    @Inject(method = "handleExplosion", at = @At("TAIL"))
+    private void handleExplosionShake(ClientboundExplodePacket packet, CallbackInfo ci) {
+        CameraShake.shakeScreenFromSource(packet.explosionSound().value(), packet.center(), packet.radius());
+    }
+}
