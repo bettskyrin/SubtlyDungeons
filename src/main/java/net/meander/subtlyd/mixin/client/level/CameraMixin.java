5e59031ca0d5a3a319a41f08efc83d13d870646f
Kyrin Betts <actions@github.com>
2026-04-23 13:58:03 -0500

    fix: GH Issue #61

--- src/main/java/net/meander/subtlyd/mixin/client/level/CameraMixin.java
+++ src/main/java/net/meander/subtlyd/mixin/client/level/CameraMixin.java
@@ -2,14 +2,14 @@ package net.meander.subtlyd.mixin.client.level;
 
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
-import net.meander.subtlyd.client.OptionInstanceSD;
 import net.meander.subtlyd.camera.CameraShake;
+import net.meander.subtlyd.client.OptionInstanceSD;
 import net.meander.subtlyd.world.entity.TentEntity;
 import net.minecraft.client.Camera;
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.util.Util;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.phys.Vec3;
 import org.jspecify.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -20,20 +20,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Environment(EnvType.CLIENT)
 @Mixin(Camera.class)
 public abstract class CameraMixin {
-    @Shadow private float yRot;
-    @Shadow private float xRot;
-
-    @Shadow protected abstract void setRotation(float y, float x);
+    @Shadow protected abstract void setRotation(float yRot, float xRot);
     @Shadow protected abstract void setPosition(double x, double y, double z);
 
-    @Shadow
-    public abstract @Nullable Entity entity();
+    @Shadow public abstract @Nullable Entity entity();
 
-    @Shadow
-    public abstract float xRot();
+    @Shadow public abstract float xRot();
+    @Shadow public abstract float yRot();
 
-    @Inject(method = "setPosition(Lnet/minecraft/world/phys/Vec3;)V", at = @At("TAIL"))
-    private void setup(Vec3 position, CallbackInfo ci) {
+    @Inject(method = "update", at = @At("TAIL"))
+    private void setup(DeltaTracker deltaTracker, CallbackInfo ci) {
         applyScreenShake();
         setCampingPlayerCamera(this.entity(), this.xRot());
     }
@@ -47,7 +43,7 @@ public abstract class CameraMixin {
             if (intensity > 0.0F) {
                 float yaw = (float) (Math.sin(Util.getMillis() / 30.0) * intensity);
                 float pitch = (float) (Math.cos(Util.getMillis() / 60.0) * intensity);
-                this.setRotation((float) (this.yRot - pitch * Math.sqrt(2)), this.xRot + (yaw));
+                this.setRotation((float) (yRot() - pitch * Math.sqrt(2)), xRot() + (yaw));
             }
         }
     }
