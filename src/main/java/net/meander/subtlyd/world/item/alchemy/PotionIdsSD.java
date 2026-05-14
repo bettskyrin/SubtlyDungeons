ecc5fac7d7b8b43591d0ba17d497c76f0b561d0b
Kyrin Betts <bettskyrin@gmail.com>
2026-05-14 18:27:04 -0500

    fix: Fixed decay namespace bug

--- src/main/java/net/meander/subtlyd/world/item/alchemy/PotionIdsSD.java
+++ src/main/java/net/meander/subtlyd/world/item/alchemy/PotionIdsSD.java
@@ -1,7 +1,7 @@
 package net.meander.subtlyd.world.item.alchemy;
 
+import net.meander.subtlyd.util.Util;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.Identifier;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.item.alchemy.Potion;
 
@@ -9,6 +9,6 @@ public class PotionIdsSD {
     public static final ResourceKey<Potion> DECAY = register("decay");
 
     private static ResourceKey<Potion> register(final String name) {
-        return ResourceKey.create(Registries.POTION, Identifier.withDefaultNamespace(name));
+        return ResourceKey.create(Registries.POTION, Util.identifier(name));
     }
 }
