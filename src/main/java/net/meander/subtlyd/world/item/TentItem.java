64e083e7edb8272e1a5adf462b214c27f6c8b013
Kyrin Betts <bettskyrin@gmail.com>
2026-06-06 14:13:33 -0500

    bump: Minecraft version bump

--- src/main/java/net/meander/subtlyd/world/item/TentItem.java
+++ src/main/java/net/meander/subtlyd/world/item/TentItem.java
@@ -9,6 +9,7 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EntitySpawnReason;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.PostSpawnProcessor;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
@@ -18,8 +19,6 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.gameevent.GameEvent;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
-
-import java.util.function.Consumer;
 
 public class TentItem extends Item {
     private final EntityType<TentEntity> entityType;
@@ -38,15 +37,15 @@ public class TentItem extends Item {
         BlockPos blockPos = blockPlaceContext.getClickedPos();
         Direction direction = useOnContext.getClickedFace();
         Vec3 vec3 = Vec3.atBottomCenterOf(blockPos);
-        AABB aABB = this.entityType.getDimensions().makeBoundingBox(vec3.x(), vec3.y(), vec3.z());
+        AABB aABB = entityType.getDimensions().makeBoundingBox(vec3.x(), vec3.y(), vec3.z());
 
         if (direction == Direction.DOWN) {
             return InteractionResult.FAIL;
         } else {
             if (level.noCollision(null, aABB) && level.getEntities(null, aABB).isEmpty()) {
                 if (level instanceof ServerLevel serverLevel) {
-                    Consumer<TentEntity> consumer = EntityType.createDefaultStackConfig(serverLevel, itemStack, player);
-                    TentEntity tentEntity = this.entityType.create(serverLevel, consumer, blockPos, EntitySpawnReason.SPAWN_ITEM_USE, true, true);
+                    PostSpawnProcessor<TentEntity> consumer = EntityType.createDefaultStackConfig(serverLevel, itemStack, player);
+                    TentEntity tentEntity = entityType.create(serverLevel, consumer, blockPos, EntitySpawnReason.SPAWN_ITEM_USE, true, true);
 
                     if (tentEntity == null) {
                         return InteractionResult.FAIL;
