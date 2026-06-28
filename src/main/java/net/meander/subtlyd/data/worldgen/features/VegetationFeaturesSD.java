0ed9d3adf0790fbccc008fb55237fa5731d4d946
Kyrin Betts <bettskyrin@gmail.com>
2026-06-28 15:43:18 -0500

    refactor: Dark forest vegetation

--- src/main/java/net/meander/subtlyd/data/worldgen/features/VegetationFeaturesSD.java
+++ src/main/java/net/meander/subtlyd/data/worldgen/features/VegetationFeaturesSD.java
@@ -2,8 +2,14 @@ package net.meander.subtlyd.data.worldgen.features;
 
 import net.meander.subtlyd.world.block.BlocksSD;
 import net.minecraft.core.Direction;
+import net.minecraft.core.Holder;
+import net.minecraft.core.HolderGetter;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.data.worldgen.features.TreeFeatures;
 import net.minecraft.data.worldgen.features.VegetationFeatures;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.TreePlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.util.random.WeightedList;
 import net.minecraft.world.level.block.Block;
@@ -13,8 +19,13 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.EnumProperty;
 import net.minecraft.world.level.block.state.properties.IntegerProperty;
 import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.RandomSelectorFeature;
 import net.minecraft.world.level.levelgen.feature.SimpleBlockFeature;
+import net.minecraft.world.level.levelgen.feature.WeightedPlacedFeature;
 import net.minecraft.world.level.levelgen.feature.stateproviders.WeightedStateProvider;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
+
+import java.util.List;
 
 /**
  * @see net.minecraft.data.worldgen.features.VegetationFeatures
@@ -22,10 +33,35 @@ import net.minecraft.world.level.levelgen.feature.stateproviders.WeightedStatePr
 public class VegetationFeaturesSD {
     public static final ResourceKey<Feature> PERSE_WILDFLOWER = FeatureUtilsSD.createKey("perse_wildflower");
     public static final ResourceKey<Feature> WILDFLOWER = FeatureUtilsSD.createKey("wildflower");
+    public static final ResourceKey<Feature> DARK_FOREST_VEGETATION = FeatureUtilsSD.createKey("dark_forest_vegetation");
 
+    @SuppressWarnings("deprecation")
     public static void bootstrap(BootstrapContext<Feature> context) {
+        HolderGetter<Feature> configuredFeatures = context.lookup(Registries.FEATURE);
+        HolderGetter<PlacedFeature> placedFeatures = context.lookup(Registries.PLACED_FEATURE);
+
+        Holder<Feature> hugeBrownMushroom = configuredFeatures.getOrThrow(TreeFeatures.HUGE_BROWN_MUSHROOM);
+        Holder<Feature> hugeRedMushroom = configuredFeatures.getOrThrow(TreeFeatures.HUGE_RED_MUSHROOM);
+        Holder<PlacedFeature> oakLeafLitter = placedFeatures.getOrThrow(TreePlacements.OAK_LEAF_LITTER);
+        Holder<PlacedFeature> fancyOakLeafLitter = placedFeatures.getOrThrow(TreePlacements.FANCY_OAK_LEAF_LITTER);
+        Holder<PlacedFeature> fallenOak = placedFeatures.getOrThrow(TreePlacements.FALLEN_OAK_TREE);
+        Holder<PlacedFeature> darkOakLeafLitter = placedFeatures.getOrThrow(TreePlacements.DARK_OAK_LEAF_LITTER);
+
         context.register(PERSE_WILDFLOWER, new SimpleBlockFeature(new WeightedStateProvider(VegetationFeatures.flowerBedPatchBuilder(BlocksSD.PERSE_WILDFLOWERS))));
         context.register(WILDFLOWER, new SimpleBlockFeature(new WeightedStateProvider(mixedFlowerBedPatchBuilder(Blocks.WILDFLOWERS, BlocksSD.PERSE_WILDFLOWERS))));
+        context.register(
+                DARK_FOREST_VEGETATION,
+                new RandomSelectorFeature(
+                        List.of(
+                                new WeightedPlacedFeature(PlacementUtils.inlinePlaced(hugeBrownMushroom), 0.00625F),
+                                new WeightedPlacedFeature(PlacementUtils.inlinePlaced(hugeRedMushroom), 0.00625F),
+                                new WeightedPlacedFeature(oakLeafLitter, 0.025F),
+                                new WeightedPlacedFeature(fallenOak, 0.00625F),
+                                new WeightedPlacedFeature(fancyOakLeafLitter, 0.025F)
+                        ),
+                        darkOakLeafLitter
+                )
+        );
     }
 
     private static WeightedList.Builder<BlockState> mixedFlowerBedPatchBuilder(final Block... flowerBedBlocks) {
--- src/main/java/net/meander/subtlyd/data/worldgen/placement/VegetationPlacementsSD.java
+++ src/main/java/net/meander/subtlyd/data/worldgen/placement/VegetationPlacementsSD.java
@@ -25,35 +25,54 @@ public class VegetationPlacementsSD {
     public static final ResourceKey<PlacedFeature> PATCH_GRASS_BIRCH_FOREST = PlacementUtilsSD.createKey("patch_grass_birch_forest");
     public static final ResourceKey<PlacedFeature> WILDFLOWERS_BIRCH_FOREST = PlacementUtilsSD.createKey("wildflowers_birch_forest");
     public static final ResourceKey<PlacedFeature> WILDFLOWERS_MEADOW = PlacementUtilsSD.createKey("wildflowers_meadow");
+    public static final ResourceKey<PlacedFeature> DARK_FOREST_VEGETATION = PlacementUtilsSD.createKey("dark_forest_vegetation");
 
     public static void bootstrap(BootstrapContext<PlacedFeature> context) {
         HolderGetter<Feature> configuredFeatures = context.lookup(Registries.FEATURE);
+        PlacementModifier treeThreshold = SurfaceWaterDepthFilter.forMaxDepth(0);
 
-        context.register(PERSE_WILDFLOWERS_DAPPLED_FOREST, new PlacedFeature(
-                configuredFeatures.getOrThrow(VegetationFeaturesSD.PERSE_WILDFLOWER),
-                List.of(
-                        RarityFilter.onAverageOnceEvery(8),
-                        InSquarePlacement.spread(),
-                        PlacementUtils.HEIGHTMAP,
-                        CountPlacement.of(32),
-                        RandomOffsetPlacement.ofTriangle(6, 2),
-                        BlockPredicateFilter.forPredicate(BlockPredicate.ONLY_IN_AIR_PREDICATE),
-                        BiomeFilter.biome())
+        context.register(PERSE_WILDFLOWERS_DAPPLED_FOREST,
+                new PlacedFeature(configuredFeatures.getOrThrow(VegetationFeaturesSD.PERSE_WILDFLOWER),
+                    List.of(
+                            RarityFilter.onAverageOnceEvery(8),
+                            InSquarePlacement.spread(),
+                            PlacementUtils.HEIGHTMAP,
+                            CountPlacement.of(32),
+                            RandomOffsetPlacement.ofTriangle(6, 2),
+                            BlockPredicateFilter.forPredicate(BlockPredicate.ONLY_IN_AIR_PREDICATE),
+                            BiomeFilter.biome()
+                    )
                 )
         );
 
-        context.register(PERSE_WILDFLOWERS_SWAMP, new PlacedFeature(
-                configuredFeatures.getOrThrow(VegetationFeaturesSD.PERSE_WILDFLOWER),
-                List.of(
-                        CountPlacement.of(2),
-                        InSquarePlacement.spread(),
-                        HeightmapPlacement.onHeightmap(Heightmap.Types.MOTION_BLOCKING),
-                        BiomeFilter.biome())
+        context.register(PERSE_WILDFLOWERS_SWAMP,
+                new PlacedFeature(configuredFeatures.getOrThrow(VegetationFeaturesSD.PERSE_WILDFLOWER),
+                    List.of(
+                            CountPlacement.of(2),
+                            InSquarePlacement.spread(),
+                            HeightmapPlacement.onHeightmap(Heightmap.Types.MOTION_BLOCKING),
+                            BiomeFilter.biome()
+                    )
                 )
         );
 
-        context.register(WILDFLOWERS_BIRCH_FOREST, new PlacedFeature(
-                configuredFeatures.getOrThrow(VegetationFeaturesSD.WILDFLOWER),
+        context.register(WILDFLOWERS_BIRCH_FOREST,
+                new PlacedFeature(configuredFeatures.getOrThrow(VegetationFeaturesSD.WILDFLOWER),
+                    List.of(
+                            CountPlacement.of(3),
+                            RarityFilter.onAverageOnceEvery(2),
+                            InSquarePlacement.spread(),
+                            PlacementUtils.HEIGHTMAP,
+                            BiomeFilter.biome(),
+                            CountPlacement.of(64),
+                            RandomOffsetPlacement.ofTriangle(6, 2),
+                            BlockPredicateFilter.forPredicate(BlockPredicate.ONLY_IN_AIR_PREDICATE)
+                    )
+                )
+        );
+
+        context.register(WILDFLOWERS_MEADOW,
+                new PlacedFeature(configuredFeatures.getOrThrow(VegetationFeaturesSD.WILDFLOWER),
                 List.of(
                         CountPlacement.of(3),
                         RarityFilter.onAverageOnceEvery(2),
@@ -66,28 +85,27 @@ public class VegetationPlacementsSD {
                 )
         );
 
-        context.register(WILDFLOWERS_MEADOW, new PlacedFeature(
-                        configuredFeatures.getOrThrow(VegetationFeaturesSD.WILDFLOWER),
-                        List.of(
-                                CountPlacement.of(3),
-                                RarityFilter.onAverageOnceEvery(2),
-                                InSquarePlacement.spread(),
-                                PlacementUtils.HEIGHTMAP,
-                                BiomeFilter.biome(),
-                                CountPlacement.of(64),
-                                RandomOffsetPlacement.ofTriangle(6, 2),
-                                BlockPredicateFilter.forPredicate(BlockPredicate.ONLY_IN_AIR_PREDICATE))
+        context.register(PATCH_GRASS_BIRCH_FOREST,
+                new PlacedFeature(configuredFeatures.getOrThrow(VegetationFeatures.GRASS),
+                    Util.copyAndAdd(
+                            VegetationPlacements.worldSurfaceSquaredWithCount(4),
+                            CountPlacement.of(16),
+                            RandomOffsetPlacement.ofTriangle(7, 3),
+                            BlockPredicateFilter.forPredicate(BlockPredicate.ONLY_IN_AIR_PREDICATE)
+                    )
                 )
         );
 
-        context.register(PATCH_GRASS_BIRCH_FOREST, new PlacedFeature(
-                configuredFeatures.getOrThrow(VegetationFeatures.GRASS),
-                Util.copyAndAdd(
-                        VegetationPlacements.worldSurfaceSquaredWithCount(4),
-                        CountPlacement.of(16),
-                        RandomOffsetPlacement.ofTriangle(7, 3),
-                        BlockPredicateFilter.forPredicate(BlockPredicate.ONLY_IN_AIR_PREDICATE)
+        context.register(DARK_FOREST_VEGETATION,
+                new PlacedFeature(configuredFeatures.getOrThrow(VegetationFeaturesSD.DARK_FOREST_VEGETATION),
+                    List.of(
+                            CountPlacement.of(32),
+                            InSquarePlacement.spread(),
+                            treeThreshold,
+                            PlacementUtils.HEIGHTMAP_OCEAN_FLOOR,
+                            BiomeFilter.biome()
+                    )
                 )
-        ));
+        );
     }
 }
