8313d38d00cca9e2e024d27cf4cd4a0640572cda
Kyrin Betts <bettskyrin@gmail.com>
2026-07-28 00:32:23 -0500

    refactor: Style

--- src/main/java/net/meander/subtlyd/data/registries/DynamicRegistriesSD.java
+++ src/main/java/net/meander/subtlyd/data/registries/DynamicRegistriesSD.java
@@ -2,6 +2,7 @@ package net.meander.subtlyd.data.registries;
 
 import net.fabricmc.fabric.api.datagen.v1.FabricPackOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricDynamicRegistryProvider;
+import net.meander.subtlyd.core.registries.RegistriesSD;
 import net.meander.subtlyd.util.UtilSD;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.core.registries.Registries;
@@ -19,7 +20,7 @@ public class DynamicRegistriesSD extends FabricDynamicRegistryProvider {
     @Override
     protected void configure(HolderLookup.Provider registries, Entries entries) {
         try {
-            entries.addAll(registries.lookupOrThrow(net.meander.subtlyd.core.registries.RegistriesSD.CAMERA_SHAKE_EVENT));
+            entries.addAll(registries.lookupOrThrow(RegistriesSD.CAMERA_SHAKE_EVENT));
             entries.addAll(registries.lookupOrThrow(Registries.ENCHANTMENT));
         } catch (Exception e) {
             UtilSD.LOGGER.error("Failed to configure dynamic registries: {}", e.getMessage());
