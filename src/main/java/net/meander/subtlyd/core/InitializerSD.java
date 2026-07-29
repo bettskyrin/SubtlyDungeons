75bccf94e0ed25e0bf35bcd48d8764d13de782eb
Kyrin Betts <bettskyrin@gmail.com>
2026-07-28 22:39:30 -0500

    refactor: Moving things around

--- src/main/java/net/meander/subtlyd/core/InitializerSD.java
+++ src/main/java/net/meander/subtlyd/core/InitializerSD.java
@@ -10,26 +10,15 @@ import net.meander.subtlyd.network.PacketNetworking;
 import net.meander.subtlyd.network.syncher.EntityDataAccessors;
 import net.meander.subtlyd.stats.StatsSD;
 import net.meander.subtlyd.util.UtilSD;
-import net.meander.subtlyd.world.level.block.BlocksSD;
-import net.meander.subtlyd.world.level.block.entity.BlockEntityTypesSD;
 import net.meander.subtlyd.world.effect.MobEffectsSD;
 import net.meander.subtlyd.world.item.ItemsSD;
 import net.meander.subtlyd.world.level.GameRulesSD;
 import net.meander.subtlyd.world.level.LevelSD;
-import net.meander.subtlyd.world.level.storage.loot.functions.LootItemFunctionsSD;
-import net.meander.subtlyd.world.level.storage.loot.predicates.LootItemConditionTypesSD;
-import net.minecraft.core.registries.BuiltInRegistries;
+import net.meander.subtlyd.world.level.block.BlocksSD;
+import net.meander.subtlyd.world.level.block.entity.BlockEntityTypesSD;
 
 @SuppressWarnings("unused")
 public class InitializerSD implements ModInitializer {
-    @Override
-    public void onInitialize() {
-        UtilSD.LOGGER.info("Initializing Subtly Dungeons");
-        server();
-        gameplay();
-        level();
-    }
-
     private void server() {
         PacketNetworking.registerCommon();
         ParticleTypesSD.registerServer();
@@ -39,8 +28,6 @@ public class InitializerSD implements ModInitializer {
     private void gameplay() {
         CriteriaTriggersSD.registration();
         StatsSD.registration();
-        LootItemConditionTypesSD.registration(BuiltInRegistries.LOOT_CONDITION_TYPE);
-        LootItemFunctionsSD.registration(BuiltInRegistries.LOOT_FUNCTION_TYPE);
         CommandsSD.registration();
         GameRulesSD.registration();
         LevelSD.registerEvents();
@@ -53,5 +40,13 @@ public class InitializerSD implements ModInitializer {
         BlocksSD.registration();
         BlockEntityTypesSD.registration();
         ItemsSD.registration();
+    }
+
+    @Override
+    public void onInitialize() {
+        UtilSD.LOGGER.info("Initializing Subtly Dungeons");
+        server();
+        gameplay();
+        level();
     }
 }
