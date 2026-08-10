93dceba9bcf7cda12c90a477c34b4b49ccad9cd4
Kyrin Betts <bettskyrin@gmail.com>
2026-08-10 03:28:48 -0500

    fix: Fixed option loading bug

--- src/main/java/net/meander/subtlyd/mixin/client/OptionsMixin.java
+++ src/main/java/net/meander/subtlyd/mixin/client/OptionsMixin.java
@@ -1,6 +1,6 @@
 package net.meander.subtlyd.mixin.client;
 
-import net.minecraft.client.Minecraft;
+import net.meander.subtlyd.client.OptionsSD;
 import net.minecraft.client.Options;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,22 +8,22 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Options.class)
-public class OptionsMixin {
+public class OptionsMixin implements OptionsSD {
     @Inject(method = "processOptions", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Options;processDumpedOptions(Lnet/minecraft/client/Options$OptionAccess;)V"))
     private void processOptions(Options.FieldAccess access, CallbackInfo ci) {
-        Options options = Minecraft.getInstance().options;
-
-        access.process("camera_shake", options.cameraShake());
-        access.process("experimental_gui", options.experimentalGui());
-        access.process("shield_crouch", options.shieldCrouch());
-        access.process("fancy_entities", options.fancyEntities());
-        access.process("shield_animation", options.shieldAnimation());
+        if (((Options) (Object) this) instanceof Options options) {
+            access.process("camera_shake", options.cameraShake());
+            access.process("experimental_gui", options.experimentalGui());
+            access.process("shield_crouch", options.shieldCrouch());
+            access.process("fancy_entities", options.fancyEntities());
+            access.process("shield_animation", options.shieldAnimation());
+        }
     }
 
     @Inject(method = "processDumpedOptions(Lnet/minecraft/client/Options$OptionAccess;)V", at = @At("HEAD"))
     private void processDumpedOptions(Options.OptionAccess access, CallbackInfo ci) {
-        Options options = Minecraft.getInstance().options;
-
-        access.process("entity_culling", options.entityCulling());
+        if (((Options) (Object) this) instanceof Options options) {
+            access.process("entity_culling", options.entityCulling());
+        }
     }
 }
